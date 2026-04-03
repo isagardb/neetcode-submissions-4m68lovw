ade7d0bf60bf15dd36743ed112eb864b4d02850d
Sagar D B <sagardb50@gmail.com>
2026-04-03 15:53:11 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i , v in enumerate(nums):
+            for j in range(i + 1, len(nums)):
+                if  target - v == nums[j]:
+                    return [i, j]
+        return [0,0]
+
+
+# TC : O(N^2)
+# SC : N(1)
+        
